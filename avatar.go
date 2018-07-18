6479d40e3a0f5a834211ce4249a61df5003fe791
Me <alifrizkipambudi@gmail.com>
2018-07-18 11:10:01 +0700

    create gravatar avatar

--- avatar.go
+++ avatar.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // ErrNoAvatarURL is the error that is returned when the
@@ -29,6 +33,24 @@ func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
 	if url, ok := c.userData["avatar_url"]; ok {
 		if urlStr, ok := url.(string); ok {
 			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
+
+// GravatarAvatar struct
+type GravatarAvatar struct{}
+
+// UseGravatar types is GravatarAvatar
+var UseGravatar GravatarAvatar
+
+// GetAvatarURL function in GravatarAvatar
+func (GravatarAvatar) GetAvatarURL(c *client) (string, error) {
+	if email, ok := c.userData["email"]; ok {
+		if emailStr, ok := email.(string); ok {
+			m := md5.New()
+			io.WriteString(m, strings.ToLower(emailStr))
+			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
 		}
 	}
 	return "", ErrNoAvatarURL
